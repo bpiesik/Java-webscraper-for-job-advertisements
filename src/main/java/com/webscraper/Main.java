a0ba75a2d0c35133449bb2da8083ab2cd3877a0f
bpiesik <112091600+bpiesik@users.noreply.github.com>
2022-09-19 15:24:35 +0200

    Update Main.java

--- src/main/java/com/webscraper/Main.java
+++ src/main/java/com/webscraper/Main.java
@@ -1,48 +1,23 @@
 package com.webscraper;
 
+import com.webscraper.model.JobAdvertisement;
+import com.webscraper.scraper.StepStoneScraper;
+import com.webscraper.scraper.WebScraper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // params
-        int pageNumber = 1;
+        WebScraper stepStone = new StepStoneScraper();
+        List<JobAdvertisement> jobAdvertisements = stepStone.getJobAdvertisements();
 
-        // get file
-        Document doc = Jsoup.connect("https://www.stepstone.de/jobs/java-backend-entwickler?" +
-                "page=" + pageNumber +
-                "&sort=2" +
-                "&action=sort_publish").get();
-
-        // get job advertisements
-        Elements ads = doc.getElementsByClass("Wrapper-sc-11673k2-0 cmgNOQ");
-
-        for (Element ad : ads) {
-            // title
-            Elements title = ad.getElementsByClass("resultlist-12iu5pk");
-            System.out.println("Title: " + title.text());
-
-            // company name
-            Elements companyName = ad.getElementsByClass("resultlist-1v262t5");
-            System.out.println("Company Name: " + companyName.text());
-
-            // location
-            Elements locations = ad.getElementsByClass("resultlist-12dlfn3");
-            System.out.println("Location: " + locations.text());
-
-            // url
-            Elements url = ad.getElementsByClass("resultlist-12iu5pk");
-            String urlTxt = "stepstone.de" + url.attr("href");
-            System.out.println("url: " + urlTxt);
-
-
-            System.out.println("\n");
+        for(JobAdvertisement ad : jobAdvertisements){
+            System.out.println(ad.getTitle());
         }
-
-
     }
 }
